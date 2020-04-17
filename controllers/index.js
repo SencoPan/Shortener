cc88cc4b5c2409cb72379333784fcb10e3af3580
SencoPan <sarprolts@gmail.com>
2020-04-17 15:23:36 +0300

    Layout styling; end

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,9 @@ const crypto = require("crypto");
 const URL = require("../models/URL");
 
 module.exports = {
+    getAllUrls: async (req, res) => {
+        await res.json(await URL.find());
+    },
     redirect: async (req, res) => {
         const {params: {shortVersion}} = req;
         const result = await URL.findOne({ shortVersion });
@@ -14,7 +17,7 @@ module.exports = {
         }
     },
     getPage: async (req, res) => {
-        res.render('shortener', {title: 'Shortener'});
+        res.render('shortener', {title: 'Shorterner'});
     },
     postURL: async (req, res) => {
         !req.session.userId ? req.session.userId = crypto.randomBytes(20).toString('hex') : false; //change to nanoid?
@@ -25,19 +28,16 @@ module.exports = {
         const existingURL = await URL.findOne({URL: url});
 
         if( !validation(url) ) {
-            res.render('shortener', {shortURL: "Введено неверное URL"});
+            res.render('shortener', {shortURL: "Введено неверное URL", type: "error"});
             return;
         } else if(existingURL){
-            res.render('shortener', {existingURL});
+            res.render('shortener', {shortURL: existingURL.shortVersion, type: "url"});
             return;
         }
 
         const shortVersion = crypto.randomBytes(5).toString('hex');
         const shortURL = await new URL({URL: url, shortVersion, userId}).save();
 
-        delete shortURL.__v;
-        delete shortURL._id;
-
-        res.render('shortener', {shortURL})
+        res.render('shortener', {shortURL: shortURL.shortVersion , type: "url"})
     },
 };
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,8 @@ const controller = require('../controllers');
 
 router.get('/', controller.getPage);
 router.post('/', controller.postURL);
-router.get('/:shortVersion', controller.redirect)
+router.get('/:shortVersion', controller.redirect);
+
+router.get('/api/getAll', controller.getAllUrls);
+
 module.exports = router;
